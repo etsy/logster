f9de9773421cf2dc0019c2cb0fc6eec3316a674a
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-11-27 20:25:21 +0000

    Format tests folder
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

--- tests/test_samplelogster.py
+++ tests/test_samplelogster.py
@@ -2,16 +2,16 @@ from logster.parsers.SampleLogster import SampleLogster
 from logster.logster_helper import LogsterParsingException
 import unittest
 
-class TestSampleLogster(unittest.TestCase):
 
+class TestSampleLogster(unittest.TestCase):
     def setUp(self):
         self.logster = SampleLogster()
 
     def test_valid_lines(self):
         access_log_tmpl = '127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" %s 2326'
-        self.logster.parse_line(access_log_tmpl % '200')
-        self.logster.parse_line(access_log_tmpl % '201')
-        self.logster.parse_line(access_log_tmpl % '400')
+        self.logster.parse_line(access_log_tmpl % "200")
+        self.logster.parse_line(access_log_tmpl % "201")
+        self.logster.parse_line(access_log_tmpl % "400")
         self.assertEqual(0, self.logster.http_1xx)
         self.assertEqual(2, self.logster.http_2xx)
         self.assertEqual(0, self.logster.http_3xx)
@@ -23,12 +23,13 @@ class TestSampleLogster(unittest.TestCase):
         metrics = self.logster.get_state(1)
         self.assertEqual(5, len(metrics))
 
-        expected = {"http_1xx": 0,
-                    "http_2xx": 2,
-                    "http_3xx": 0,
-                    "http_4xx": 1,
-                    "http_5xx": 0
-                   }
+        expected = {
+            "http_1xx": 0,
+            "http_2xx": 2,
+            "http_3xx": 0,
+            "http_4xx": 1,
+            "http_5xx": 0,
+        }
         for m in metrics:
             self.assertEqual(expected[m.name], m.value)
 
@@ -37,18 +38,21 @@ class TestSampleLogster(unittest.TestCase):
         metrics = self.logster.get_state(2)
         self.assertEqual(5, len(metrics))
 
-        expected = {"http_1xx": 0,
-                    "http_2xx": 1,
-                    "http_3xx": 0,
-                    "http_4xx": 0.5,
-                    "http_5xx": 0
-                   }
+        expected = {
+            "http_1xx": 0,
+            "http_2xx": 1,
+            "http_3xx": 0,
+            "http_4xx": 0.5,
+            "http_5xx": 0,
+        }
         for m in metrics:
             self.assertEqual(expected[m.name], m.value)
 
     def test_invalid_line(self):
-        self.assertRaises(LogsterParsingException, self.logster.parse_line, 'invalid log entry')
+        self.assertRaises(
+            LogsterParsingException, self.logster.parse_line, "invalid log entry"
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
-
