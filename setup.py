ac96c17501b2f21475e05210808d56f122d3553c
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-11-27 20:25:29 +0000

    Format setup.py
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -7,23 +7,14 @@ except ImportError:
 
 
 setup(
-    name='logster',
-    version='1.0.1',
-    description='Parse log files, generate metrics for Graphite and Ganglia',
-    author='Etsy',
-    url='https://github.com/etsy/logster',
-    packages=[
-        'logster',
-        'logster/parsers',
-        'logster/tailers',
-        'logster/outputs'
-    ],
-    install_requires = [
-        'pygtail>=0.5.1'
-    ],
+    name="logster",
+    version="1.0.1",
+    description="Parse log files, generate metrics for Graphite and Ganglia",
+    author="Etsy",
+    url="https://github.com/etsy/logster",
+    packages=["logster", "logster/parsers", "logster/tailers", "logster/outputs"],
+    install_requires=["pygtail>=0.5.1"],
     zip_safe=False,
-    scripts=[
-        'bin/logster'
-    ],
-    license='GPL3',
+    scripts=["bin/logster"],
+    license="GPL3",
 )
